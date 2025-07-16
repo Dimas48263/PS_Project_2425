f0367d8c20637488af95074271d888b3640583e7
Luis Alves - a46974 <a46974@alunos.isel.pt>
2025-07-16 13:55:01 +0100

    feat: html manual draft

--- Frontend/zcap_net/lib/shared/security_utils.dart
+++ Frontend/zcap_net/lib/shared/security_utils.dart
@@ -1,14 +1,5 @@
 
 import 'package:bcrypt/bcrypt.dart';
-import 'package:crypto/crypto.dart';
-import 'dart:convert';
-
-//old, using sha256.
-String encryptPassword(String password) {
-  final bytes = utf8.encode(password);
-  final digest = sha256.convert(bytes);
-  return digest.toString();
-}
 
 //New@05-07-2025: using same algorithm as API
 String hashPassword(String password) {
